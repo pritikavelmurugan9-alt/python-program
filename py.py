d6cd561cce5d82eb4db633a6867551a76f4db9d6
PRITIKA.V <pritikavelmurugan9@gmail.com>
2025-09-10 17:40:37 +0530

    Add files via upload

---
+++ py.py
@@ -0,0 +1,15 @@
+d={"emno":26,"emname":"book"}
+print(d)
+print(d["emno"])
+print(d.get("emname"))
+d["emname"]="pritika"
+print(d)
+d["age"]=18
+for key in d:
+    print(key)
+for key,value in d.items():
+    print(key,":",value)
+d.pop
+print(d)
+
+      
