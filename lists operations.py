83cf85f34b1fefb510f4b06942d999e860205b60
PRITIKA.V <pritikavelmurugan9@gmail.com>
2025-09-10 17:47:05 +0530

    Add files via upload

---
+++ lists operations.py
@@ -0,0 +1,11 @@
+a=[]
+n=int(input("enter the nimbers to be added in the list"))
+for i in range (0,n):
+    m=int(input("enter the numbers"))
+    a.append(m)
+print(a)
+print(a[0])
+print(a[-1])
+print(a[0:])
+print(a[])
+
